8b5290cb1ea941d7b7f402897471ccf9e1ecd7d7
Esam Mahmoud-Ahmed <55792105+CODman-2019@users.noreply.github.com>
2023-10-18 13:44:23 -0400

    preparing code

---
+++ Source/LC_CursedMatch/Game.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Game.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LC_CURSEDMATCH_API AGame : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LC_CursedMatch/GameSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+#include "GameSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LC_CURSEDMATCH_API AGameSettings : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+};
